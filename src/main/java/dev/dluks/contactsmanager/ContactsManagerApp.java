c355b40a7f8f4a2e630a6d15b603503654f322aa
Diogo Oliveira <dluks82@gmail.com>
2024-08-25 22:23:58 -0300

    chore: initial commit

---
+++ src/main/java/dev/dluks/contactsmanager/ContactsManagerApp.java
@@ -0,0 +1,7 @@
+package dev.dluks.contactsmanager;
+
+public class ContactsManagerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Contacts Manager App");
+    }
+}
