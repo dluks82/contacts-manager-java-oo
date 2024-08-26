642bdb9b7418d1efb2a50177eece0bfe0a146120
Diogo Oliveira <dluks82@gmail.com>
2024-08-25 23:14:43 -0300

    Add `Junit` and `sample test`'

---
+++ src/test/java/dev/dluks/contactsmanager/ContactsManagerAppTest.java
@@ -0,0 +1,16 @@
+package dev.dluks.contactsmanager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactsManagerAppTest {
+
+    @Test
+    public void test(){
+        boolean expected = true;
+
+        assertTrue(expected);
+    }
+
+}
