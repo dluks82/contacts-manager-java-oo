7d2d407e9f738f2b512e60a6daa768b09ba105db
Diogo Oliveira <dluks82@gmail.com>
2024-08-26 15:05:53 -0300

    Add `DatabaseInitializer` for main

---
+++ src/main/java/dev/dluks/contactsmanager/DatabaseInitializer.java
@@ -0,0 +1,10 @@
+package dev.dluks.contactsmanager;
+
+import java.sql.SQLException;
+
+public class DatabaseInitializer {
+
+    public static void execute(IDatabase database) throws SQLException {
+        database.executeScript("schema.sql");
+    }
+}
