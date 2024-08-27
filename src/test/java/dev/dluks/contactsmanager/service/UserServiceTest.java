0b76b164d24ad634558f9bd5d4b67ea6d665e4f4
Diogo Oliveira <dluks82@gmail.com>
2024-08-27 20:34:09 -0300

    feat: Add `UserService` and `tests`

---
+++ src/test/java/dev/dluks/contactsmanager/service/UserServiceTest.java
@@ -0,0 +1,93 @@
+package dev.dluks.contactsmanager.service;
+
+import dev.dluks.contactsmanager.dao.UserDAO;
+import dev.dluks.contactsmanager.db.DatabaseInitializer;
+import dev.dluks.contactsmanager.db.H2InMemoryDatabase;
+import dev.dluks.contactsmanager.dto.LoginRequestDTO;
+import dev.dluks.contactsmanager.dto.LoginResponseDTO;
+import dev.dluks.contactsmanager.dto.RegisterUserRequestDTO;
+import dev.dluks.contactsmanager.dto.RegisterUserResponseDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    H2InMemoryDatabase db = new H2InMemoryDatabase();
+    Connection connection;
+    UserDAO userDAO;
+
+    UserService sut;
+
+    @BeforeEach
+    public void setup() {
+        try {
+            connection = db.getConnection();
+
+            try (Statement stmt = connection.createStatement()) {
+                stmt.execute("DROP TABLE IF EXISTS users");
+            }
+
+            DatabaseInitializer.execute(db);
+
+            userDAO = new UserDAO(connection);
+            sut = new UserService(userDAO);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void shouldBeAbleToRegisterNewUser() {
+        RegisterUserRequestDTO newUser = new RegisterUserRequestDTO("Diogo", "diogo", "123");
+        RegisterUserResponseDTO response = sut.register(newUser);
+        assertTrue(response.created());
+    }
+
+    @Test
+    public void shouldBeAbleToLogin() {
+        RegisterUserRequestDTO newUser = new RegisterUserRequestDTO("Diogo", "diogo", "123");
+        sut.register(newUser);
+
+        LoginRequestDTO loginRequestDTO = new LoginRequestDTO("diogo", "123");
+
+        LoginResponseDTO loginResponseDTO = sut.login(loginRequestDTO);
+
+        assertNotNull(loginResponseDTO);
+        assertEquals(loginResponseDTO.name(), newUser.name());
+        assertEquals(loginResponseDTO.id(), 1L);
+    }
+
+    @Test
+    public void shouldNotBeAbleToLoginWithInvalidLogin() {
+        RegisterUserRequestDTO newUser = new RegisterUserRequestDTO("Diogo", "diogo", "123");
+
+        String invalidLogin = "wrongLogin";
+        sut.register(newUser);
+
+        LoginRequestDTO loginRequestDTO = new LoginRequestDTO(invalidLogin, "123");
+
+        LoginResponseDTO loginResponseDTO = sut.login(loginRequestDTO);
+
+        assertNull(loginResponseDTO);
+    }
+
+    @Test
+    public void shouldNotBeAbleToLoginWithWrongPassword() {
+        RegisterUserRequestDTO newUser = new RegisterUserRequestDTO("Diogo", "diogo", "123");
+
+        String wrongPassword = "wrongPassword";
+        sut.register(newUser);
+
+        LoginRequestDTO loginRequestDTO = new LoginRequestDTO("diogo", wrongPassword);
+
+        LoginResponseDTO loginResponseDTO = sut.login(loginRequestDTO);
+
+        assertNull(loginResponseDTO);
+    }
+}
